2e97dd8629244b067dcc2fb2bd00ceb398066c52
Radek Burget <burgetr@fit.vutbr.cz>
2016-01-04 13:33:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+CSSBox - Testing framework
+==========================
+A testing framework for the [https://github.com/radkovo/CSSBox](CSSBox rendering engine)
+(c) 2016 Radek Burget (burgetr@fit.vutbr.cz)
---
+++ src/main/java/org/fit/cssbox/test/ReferenceTest.java
@@ -0,0 +1,80 @@
+/**
+ * ReferenceComparisonTest.java
+ *
+ * Created on 4. 1. 2016, 11:06:09 by burgetr
+ */
+package org.fit.cssbox.test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import org.fit.cssbox.test.ReferenceResults;
+import org.fit.cssbox.test.TestBatch;
+
+/**
+ * 
+ * @author burgetr
+ */
+public class ReferenceTest
+{
+
+    public void compareWithReference() throws MalformedURLException
+    { 
+        ReferenceResults ref = new ReferenceResults();
+        URL url = new URL("file://" + System.getProperty("user.home") + "/tmp/css-test-built/css21_dev/html4/");
+        TestBatch tester = new TestBatch(url);
+        
+        tester.runTests();
+        Map<String, Float> results = tester.getResults();
+
+        System.out.println("Regressions:");
+        int errorcnt = 0;
+        for (Map.Entry<String, Float> item : results.entrySet())
+        {
+            String name = item.getKey();
+            float val = item.getValue();
+            float refval = ref.get(name);
+            if (val < 0.0f)
+            {
+                System.out.println(name + " : execution failed");
+                errorcnt++;
+            }
+            if (val > refval)
+            {
+                System.out.println(name + " : regression found (" + val + " > " + refval + ")");
+                errorcnt++;
+            }
+        }
+        
+        System.out.println("Regressions:");
+        int progcnt = 0;
+        for (Map.Entry<String, Float> item : results.entrySet())
+        {
+            String name = item.getKey();
+            float val = item.getValue();
+            float refval = ref.get(name);
+            if (val >= 0.0f && val < refval)
+            {
+                System.out.println(name + " : progression found (" + val + " < " + refval + ")");
+                progcnt++;
+            }
+        }
+        System.out.println();
+        System.out.println("Total " + errorcnt + " regressions");
+        System.out.println("Total " + progcnt + " progressions");
+
+    }
+    
+    public static void main(String[] args)
+    {
+        ReferenceTest test = new ReferenceTest();
+        try
+        {
+            test.compareWithReference();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
